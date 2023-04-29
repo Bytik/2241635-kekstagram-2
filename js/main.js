c9b11fcc048efb534964a040748a1f3ed8f1521a
IlyaML <KolyasnikoFF.ilya@yandex.ru>
2023-04-29 18:27:40 +0500

    update 2 function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
 // имя_функции(проверяемая_строка, максимальная_длина); // Результат: true, если строка проходит по длине, и false — если не проходит
-function stringLength(stringCheck, stringMaxLength) {
-  return stringMaxLength >= stringCheck.length;
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
